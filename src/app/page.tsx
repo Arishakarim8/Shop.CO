dfe0adead8e1cd923feabb0c8373a0d513a85c18
Arisha karim <arishakarim8@gmail.com>
2025-02-05 13:04:33 +0500

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Navbar from "@/components/Navbar";
-import BrandName from '@/components/Header';
 import Hero from "@/components/Hero";
 import NewArrivals from "@/components/NewArrivals";
 import TopSelling from "@/components/TopSelling";
 import Reviews from "@/components/Reviews";
-import Footer from "./Footer/page";
 export default function Hone(){
   return (
     <div className=" ">
@@ -12,9 +9,6 @@ export default function Hone(){
       <NewArrivals />
       <TopSelling /> 
       <Reviews />
-      {/* <div className="top-[3480px] absolute md:top-[3781px]"> */}
-        {/* <Footer /> */}
-      {/* </div> */}
     </div>
   )
 }
